e681fbc52ed97a07ac6d733052e68f98e6df585b
evacheliotis <evmorfia.cheliotis@dawsoncollege.qc.ca>
2024-09-12 20:46:08 +0000

    cake

---
+++ cake.py
@@ -0,0 +1,19 @@
+import math
+
+def getCylinderVolume(radius, height) :
+    volume = math.pi * (radius ** 2)  * height
+    return volume 
+
+x = getCylinderVolume(10, 12)
+y = getCylinderVolume(2, 6)
+print (round(x,4))
+print (round(y,4))
+print(int(x))
+
+def getNumberOfSlices(radius, height, volumeOfSlice) :
+    volume = height * (radius ** 2) * math.pi
+    numberOfSlices = volume / volumeOfSlice
+    return int(numberOfSlices)
+
+numberOfSlices1 = getNumberOfSlices(10, 10, 10)
+print(numberOfSlices1)
